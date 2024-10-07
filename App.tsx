967e9b07e4580ef6b970231a59eccfcce565143a
Rickard Natt och Dag <rickard.natt-och-dag@seb.se>
2024-10-07 09:10:59 +0200

    chore: reproduce bug

--- App.tsx
+++ App.tsx
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import Animated, {
+	SharedValue,
+	useAnimatedProps,
+	useAnimatedStyle,
+	useSharedValue,
+} from "react-native-reanimated";
+
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+
+function Button({
+	index,
+	page,
+	text,
+}: { index: number; page: SharedValue<number>; text: string }) {
+	const buttonBackground = useAnimatedStyle(() => {
+		const isDisabled = page.value === index;
+
+		return {
+			backgroundColor: isDisabled ? "lightgrey" : "lightgreen",
+		};
+	});
+
+	const animatedProps = useAnimatedProps(() => {
+		return {
+			disabled: page.value === index,
+		};
+	});
+
+	return (
+		<AnimatedPressable
+			animatedProps={animatedProps}
+			style={[styles.button, buttonBackground]}
+			onPress={() => (page.value = index)}
+		>
+			<Text>{text}</Text>
+		</AnimatedPressable>
+	);
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const page = useSharedValue(0);
+
+	return (
+		<View style={styles.container}>
+			<Button index={0} page={page} text="First" />
+			<Button index={1} page={page} text="Second" />
+			<Button index={2} page={page} text="Third" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		flexDirection: "row",
+		columnGap: 16,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	button: {
+		padding: 12,
+	},
 });
